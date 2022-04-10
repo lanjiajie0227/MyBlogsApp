0e13a1aab0c4b07191c1bf49e5ce338a29b5be25
lanji <lanji@LANJIAJIE>
2022-04-11 00:09:16 +0800

    添加项目文件。

---
+++ MasaBlazorApp/Pages/EditBlog/EditBlogs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MasaBlazorApp.Pages.EditBlog
+{
+    public class EditBlogsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
